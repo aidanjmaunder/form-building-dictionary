f2bd83c7e1d4545c5a95a3f8a7cae01bfe3881fb
scurrilous <aidan.maunder@gmail.com>
2021-08-24 16:03:20 -0400

    v1 almost complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <i className="fas fa-book" style={{marginTop: "-40vh", marginBottom: "3rem", fontSize: "6rem"}}></i>
         <AutoDictionary />
       </header>
     </div>

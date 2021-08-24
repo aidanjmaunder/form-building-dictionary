6a9e9c8f6dac1c2277645f1d5c461b1cbb6d4ae9
aidan maunder <aidan.maunder@gmail.com>
2021-08-24 10:25:08 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AutoDictionary from "./AutoDictionary";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <AutoDictionary />
+      </header>
+    </div>
+  );
+}
+
+export default App;
